9a59724ce2a481ecc89513463af093a21a202a32
Mikira Sora <mikirasora0409@126.com>
2019-03-06 21:56:10 +0800

    add generic lyrics parser in core

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using LyricsFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cont = @"
+[00:12.00]Line 1 lyrics
+[00:17.20][04:17.20]Line 2 lyrics
+[00:21.10][01:17.20][02:17.20]Line 3 lyrics
+[00:12.00]Line 4 lyrics
+";
+
+            var zz = LyricsParser.Parse(cont).OrderBy(x=>x.StartTime);
+
+            foreach (var z in zz)
+            {
+                Console.WriteLine(z);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LyricsFinder/Lyrics/Parser/LyricsParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LyricsFinder
+{
+    public static class LyricsParser
+    {
+        const string TIMELINE_REGEX = @"\[(\d+\:)+\d+(\.\d+)?\]";
+        private static Regex timeline_parse_regex = new Regex(@"\[(\d+\d*)\:(\d+)\.(\d*)?\]");
+        private static Regex timeline_regex = new Regex(TIMELINE_REGEX);
+        private static Regex lyric_regex = new Regex($"(({TIMELINE_REGEX})+)(.*)");
+
+        public static IEnumerable<Sentence> Parse(string lyrics_content)
+        {
+            using (var reader=new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(lyrics_content))))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    var match = lyric_regex.Match(line);
+
+                    if (!match.Success)
+                        continue;
+
+                    var content = match.Groups[5].Value;
+
+                    var timeline_matches = timeline_regex.Matches(match.Groups[1].Value);
+
+                    foreach (Match m in timeline_matches)
+                    {
+                        var zz = timeline_parse_regex.Match(m.Groups[0].Value);
+                        int min = int.Parse(zz.Groups[1].Value.ToString()), sec = int.Parse(zz.Groups[2].Value.ToString()), msec = int.Parse(zz.Groups[3].Value.ToString());
+
+                        int time = min*60*1000+sec*1000+msec;
+
+                        Sentence sentence =new Sentence(content, time);
+
+                        yield return sentence;
+                    }
+                }
+            }
+        }
+    }
+}
