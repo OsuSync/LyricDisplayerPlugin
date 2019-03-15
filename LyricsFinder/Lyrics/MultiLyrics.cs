3f42d4d1793d0c25545aed1489daac6577fa7a28
Mikira Sora <mikirasora0409@126.com>
2019-03-15 12:40:24 +0800

    fix crash by empty lyrics in MultiLyrics

--- LyricsFinder/Lyrics/MultiLyrics.cs
+++ LyricsFinder/Lyrics/MultiLyrics.cs
@@ -15,24 +15,22 @@ namespace LyricsFinder
 
         public override (Sentence, int) GetCurrentSentence(int time)
         {
-            int min_index = int.MaxValue;
-            Sentence sentence = Sentence.Empty;
+            var result = lyricses.Select(l => l.GetCurrentSentence(time));
 
-            foreach (var lyrics in lyricses)
-            {
-                var result = lyrics.GetCurrentSentence(time);
+            if (!result.Any())
+                return (Sentence.Empty, 0);
 
-                if (result.Item1.Equals(Sentence.Empty))
-                    continue;
+            //如果其中一份歌词返回空的，那说明这里有停顿(?
+            //比如 https://puu.sh/D0bRh/569789535e.png
+            if (result.Any(x=>x.Item1.Equals(Sentence.Empty)))
+                return (Sentence.Empty, result.Min(x => x.Item2));
 
-                sentence=sentence+result.Item1;
-                min_index=Math.Min(min_index, result.Item2);
-            }
+            var ret = (result.Aggregate((z, x) => (z.Item1+x.Item1, 0)).Item1, result.Min(x => x.Item2));
 
             //trim newline in sentence content
-            sentence.Content=sentence.Content.Trim('\n', '\r');
+            ret.Item1.Content=ret.Item1.Content.Trim('\n', '\r');
 
-            return (sentence, min_index);
+            return ret;
         }
     }
 }
